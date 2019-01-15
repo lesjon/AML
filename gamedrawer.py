8b9cf16e82a2b6909b751a004d5d10c36229afe4
Unknown <leonklute@live.nl>
2019-01-15 17:17:24 +0100

    draw a whole game in real time using gamedrawer

---
+++ gamedrawer.py
@@ -0,0 +1,74 @@
+from tkinter import *
+from time import sleep
+
+class DrawGame:
+    master = None
+    canvas = None
+    width = 1200
+    height = 900
+    field_width = 12/4
+    field_height = 9/4
+
+    def __init__(self):
+
+        def _create_circle(self, x, y, r, **kwargs):
+            return self.create_oval(x-r, y-r, x+r, y+r, **kwargs)
+        Canvas.create_circle = _create_circle
+
+        self.master = Tk()
+        self.scroll_bar = Scrollbar(self.master, orient="horizontal", length=self.width * 2/3)
+        self.scroll_bar.pack()
+
+        self.canvas = Canvas(self.master, width=self.width, height=self.height)
+        self.canvas.pack()
+        '''
+        self.canvas.create_line(0, 0, 200, 100)
+        self.canvas.create_line(0, 100, 200, 0, fill="red", dash=(4, 4))
+
+        self.canvas.create_rectangle(50, 25, 150, 75, fill="blue")
+        '''
+        self.draw_robots(None)
+        self.update()
+
+    def update(self):
+        self.master.update()
+
+    @staticmethod
+    def wait_till_close():
+        mainloop()
+
+    def draw_robots(self, data):
+        self.canvas.create_circle(100, 120, 50, fill="blue", outline="#DDD", width=4)
+
+    def draw_game_from_json(self, json_file):
+        print("draw game from json")
+        for json_object in json_file:
+            self.draw_json(json_object)
+            self.update()
+            self.clear_canvas()
+            sleep(1/60)
+            '''self.master.after(1000/60, self.draw_game_from_json)
+            yield'''
+
+    def draw_json(self, json_data):
+
+        def draw_object(game_object, size, color):
+            vel_vector_factor = 0.01
+            x_pos = (game_object['x'] + self.field_width / 2) * self.width / self.field_width
+            y_pos = (game_object['y'] + self.field_height / 2) * self.height / self.field_height
+            x_vel = (game_object['x_vel'] + self.field_width / 2) * self.width / self.field_width
+            y_vel = (game_object['y_vel'] + self.field_height / 2) * self.height / self.field_height
+            y_vel *= vel_vector_factor
+            x_vel *= vel_vector_factor
+            self.canvas.create_circle(x_pos, y_pos, size, fill=color)
+            self.canvas.create_line(x_pos, y_pos, x_pos + x_vel, y_pos + y_vel, width=1)
+
+        for robot in json_data['robots_yellow']:
+            draw_object(robot, 10, "yellow")
+        for robot in json_data['robots_blue']:
+            draw_object(robot, 10, "blue")
+        for ball in json_data['balls']:
+            draw_object(ball, 4, "orange")
+
+    def clear_canvas(self):
+        self.canvas.delete("all")
--- main.py
+++ main.py
@@ -3,25 +3,93 @@ import pandas
 import matplotlib.pyplot as plt
 import tensorflow as tf
 import json
+from gamedrawer import DrawGame
+
+
+def json2input(path):
+    def dict2lists(dict_to_split):
+        k, v = list(dict_to_split.keys()), list(dict_to_split.values())
+        # print(list(zip(k, v)))
+        return k, v
+
+    read_file = open(path, "r")
+    json_file = json.load(read_file)
+    return_list = []
+    keys, robots_yellow_keys, robots_blue_keys, balls_keys = [], [], [], []
+
+    for json_object in json_file:
+        keys, values = dict2lists(json_object)
+
+        robots_yellow_keys, robots_yellow_values, robots_blue_keys, robots_blue_values, balls_keys = [], [], [], [], []
+
+        for robot in values[keys.index("robots_yellow")]:
+            k, v = dict2lists(robot)
+            robots_yellow_keys.extend(k[1:])
+            robots_yellow_values.extend(v[1:])
+
+        for robot in values[keys.index("robots_blue")]:
+            k, v = dict2lists(robot)
+            robots_blue_keys.extend(k[1:])
+            robots_blue_values.extend(v[1:])
+
+        balls_keys, balls_values = dict2lists(values[keys.index("balls")][0])
+
+        return_list.append(robots_yellow_values + robots_blue_values + balls_values)
+
+    return return_list, robots_yellow_keys + robots_blue_keys + balls_keys
 
 
 if __name__ == '__main__':
     print("Starting project!")
+    dg = DrawGame()
 
-    read_file = open("logs/log.json", "r")
-    data = json.load(read_file)
-#    for sample in data:
-#        print(sample)
+    read_file = open("logs/testWriterOutput.json", "r")
+    json_file = json.load(read_file)
+
+    dg.draw_game_from_json(json_file)
+
+    print("load data from json")
+    data, data_keys = json2input("logs/testWriterOutput.json")
+
+    print(list(zip(data_keys, data[0])))
+
+    index = int(0.8*len(data))
+    data_for_train = data[:index]
+    data_for_test = data[index:]
+
+    (data_train, category_train) = data_for_train[:-1], data_for_train[1:]
+    (data_test, category_test) = data_for_test[:-1],  data_for_test[1:]
+    dg.wait_till_close()
+    exit()
+    print("create model")
+    model = tf.keras.models.Sequential([
+        tf.keras.layers.Flatten(),
+        tf.keras.layers.Dense(len(data_train[0]), activation=tf.keras.activations.linear),  # tf.nn.relu),
+        tf.keras.layers.Dropout(0.2),
+        tf.keras.layers.Dense(len(category_train[0]), activation=tf.keras.activations.linear)
+    ])
+
+    print(model)
+    print("compile model")
+    model.compile(optimizer='adam',
+                  loss=tf.losses.mean_squared_error,
+                  metrics=['accuracy'])
+
+    model.fit(data_train, category_train, epochs=5, verbose=2)
+    model.evaluate(data_test, category_test)
+
+    exit()
+
     mnist = tf.keras.datasets.mnist
 #    print(mnist.load_data())
 #    print(mnist.load_data()[0][0])
 #    print(mnist.load_data()[0][0][0])
-    (x_train, y_train),(x_test, y_test) = mnist.load_data()
+    (data_train, category_train), (data_test, category_test) = mnist.load_data()
 
-    x_train, x_test = x_train / 255.0, x_test / 255.0
+    data_train, data_test = data_train / 255.0, data_test / 255.0
 
-    print(x_train[0])
-    print(y_train)
+    print(data_train[0])
+    print(category_train)
     exit()
 
     model = tf.keras.models.Sequential([
@@ -34,5 +102,5 @@ if __name__ == '__main__':
                   loss='sparse_categorical_crossentropy',
                   metrics=['accuracy'])
 
-    model.fit(x_train, y_train, epochs=5)
-    model.evaluate(x_test, y_test)
+    model.fit(data_train, category_train, epochs=5)
+    model.evaluate(data_test, category_test)
