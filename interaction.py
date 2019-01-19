46065a0d93b39fd2dc6089b53804534e07dc6e57
Nahuel <na.manterola@gmail.com>
2019-01-19 14:13:05 +0100

    Now works with Emiel's cut logs

--- interaction.py
+++ interaction.py
@@ -7,56 +7,57 @@ import gamedrawer
 import random
 
 
-# Takes the file from string 'resource' and transforms the data into a 4 dimensional array: dataOrdered[frame][team1, team2, ball][robotID][x,y,x_vel,etc]
+# Takes the file from string 'resource' and transforms the data into a 4 dimensional array: dataOrdered[set][frame][team1, team2, ball][robotID][x,y,x_vel,etc]
 def file_to_tensor(resource):
     read_file = open(resource, "r")
     data = json.load(read_file)
-    data = data["data"]
     dataOrdered = []
-    for i in data:
-        if len(i["robots_yellow"]) == 8:
-            dataOrdered.append([])
+    for set in data:
+        dataOrdered.append([])
+        for frame in set:
             dataOrdered[-1].append([])
-            for j in i["robots_yellow"]:
-                dataOrdered[-1][-1].append([])
-                dataOrdered[-1][-1][-1].append(j["x"])
-                dataOrdered[-1][-1][-1].append(j["y"])
-                dataOrdered[-1][-1][-1].append(j["x_vel"]/6000)
-                dataOrdered[-1][-1][-1].append(j["y_vel"]/4500)
-                dataOrdered[-1][-1][-1].append(j["x_orien"])
-                dataOrdered[-1][-1][-1].append(j["y_orien"])
-            dataOrdered[-1].append([])
-            for j in i["robots_blue"]:
-                dataOrdered[-1][-1].append([])
-                dataOrdered[-1][-1][-1].append(j["x"])
-                dataOrdered[-1][-1][-1].append(j["y"])
-                dataOrdered[-1][-1][-1].append(j["x_vel"]/6000)
-                dataOrdered[-1][-1][-1].append(j["y_vel"]/4500)
-                dataOrdered[-1][-1][-1].append(j["x_orien"])
-                dataOrdered[-1][-1][-1].append(j["y_orien"])
-            dataOrdered[-1].append([])
-            for j in i["balls"]:
-                dataOrdered[-1][-1].append([])
-                dataOrdered[-1][-1][-1].append(j["x"])
-                dataOrdered[-1][-1][-1].append(j["y"])
-                dataOrdered[-1][-1][-1].append(j["x_vel"])
-                dataOrdered[-1][-1][-1].append(j["y_vel"])
+            dataOrdered[-1][-1].append([])
+            for j in frame["robots_yellow"]:
+                dataOrdered[-1][-1][-1].append([])
+                dataOrdered[-1][-1][-1][-1].append(j["x"])
+                dataOrdered[-1][-1][-1][-1].append(j["y"])
+                dataOrdered[-1][-1][-1][-1].append(j["x_vel"])
+                dataOrdered[-1][-1][-1][-1].append(j["y_vel"])
+                dataOrdered[-1][-1][-1][-1].append(j["x_orien"])
+                dataOrdered[-1][-1][-1][-1].append(j["y_orien"])
+            dataOrdered[-1][-1].append([])
+            for j in frame["robots_blue"]:
+                dataOrdered[-1][-1][-1].append([])
+                dataOrdered[-1][-1][-1][-1].append(j["x"])
+                dataOrdered[-1][-1][-1][-1].append(j["y"])
+                dataOrdered[-1][-1][-1][-1].append(j["x_vel"])
+                dataOrdered[-1][-1][-1][-1].append(j["y_vel"])
+                dataOrdered[-1][-1][-1][-1].append(j["x_orien"])
+                dataOrdered[-1][-1][-1][-1].append(j["y_orien"])
+            dataOrdered[-1][-1].append([])
+            for j in frame["balls"]:
+                dataOrdered[-1][-1][-1].append([])
+                dataOrdered[-1][-1][-1][-1].append(j["x"])
+                dataOrdered[-1][-1][-1][-1].append(j["y"])
+                dataOrdered[-1][-1][-1][-1].append(j["x_vel"])
+                dataOrdered[-1][-1][-1][-1].append(j["y_vel"])
     len1 = len(dataOrdered)
     len2 = len(dataOrdered[0])
     len3 = len(dataOrdered[0][0])
     len4 = len(dataOrdered[0][0][0])
-    npData = np.zeros((len1,len2,len3,len4))
+    len5 = len(dataOrdered[0][0][0][0])
+    npData = np.zeros((len1,len2,len3,len4, len5))
     for i in range(len1):
         for j in range(len2):
             for k in range(len3):
                 for l in range(len4):
-                    try:
-                        npData[i, j, k, l] = dataOrdered[i][j][k][l]
-                    except:
-                        pass
+                    for m in range(len5):
+                        try:
+                            npData[i, j, k, l, m] = dataOrdered[i][j][k][l][m]
+                        except:
+                            pass
     return npData
 
-# @param: 'input' should contain a TF array with the data of the source robot and the target robot + info on whether the target is an opponent
 def train_opponent(training_robot, opponent, weights1, weights2, weights3, weights4):
     input = tf.reshape(tf.concat([training_robot, opponent], 0), [12,1])
     h1 = tf.matmul(tf.transpose(input), weights1)
@@ -93,10 +94,10 @@ def train_cycle(frame, w_h1_o, w_o1_o, w_h1_t, w_o1_t, w_h1_b, w_o1_b, w_h2_o, w
     return state
 
 
-data = file_to_tensor('Resources/Logs/RD_RT.json')        # Load data from file
+data = file_to_tensor('Resources/LogsCut/2018-06-19_19-24_CMμs-vs-TIGERs_Mannheim.json')        # Load data from file
 predict_frames = 30                                        # Amount of frames to predict into the future.
-x = data[:-predict_frames]
-y = data[predict_frames:]
+x = data[:,:-predict_frames]
+y = data[:,predict_frames:]
 # y = np.stack ( np.array([np.array([np.array([np.array(yk) for yk in yj]) for yj in yi]) for yi in y]) )
 
 tf_x = tf.placeholder(tf.float32, shape=(3, 8, 6))  # Placeholder for input data of network
@@ -119,22 +120,24 @@ py_x = train_cycle(tf_x, w_h1_o, w_o1_o, w_h1_t, w_o1_t, w_h1_b, w_o1_b, w_h2_o,
 
 loss = tf.losses.mean_squared_error(tf_y, py_x)                             # Loss is calculated taking the mean of the squared error of the output
 train_op = tf.train.AdamOptimizer(1).minimize(loss)                          # Trained with gradient descent
-dg = gamedrawer.GameDrawer()
+# dg = gamedrawer.GameDrawer()
 # initialize the jsonGameProcessor with the appropriate keys
-RD_RT = jsonGameProcessor.JsonToArray('Resources/Logs/RD_RT.json')
+# RD_RT = jsonGameProcessor.JsonToArray('Resources/Logs/RD_RT.json')
 counter = 0
 with tf.Session() as sess:
     tf.global_variables_initializer().run()
     for i in range(1,10000):
-        for frame in range(len(x)-100):
-            _, calcLoss, py, ty = sess.run([train_op, loss, py_x, tf_y], feed_dict={tf_x: x[frame], tf_y: y[frame][0]})
-            if frame == counter:
-                py = py.flatten()
-                ty = ty.flatten()
-                py = np.append(ty,py)
-                dg.clear_canvas()
-                dg.draw_game_from_nparray(py)
-        counter += 1
-        print(i, " Loss: ", calcLoss)
+        for set in range(len(x)):
+            for frame in range(len(x[set])):
+                _, calcLoss, py, ty = sess.run([train_op, loss, py_x, tf_y], feed_dict={tf_x: x[set,frame], tf_y: y[set,frame][0]})
+                # if frame == counter:
+                    # py = py.flatten()
+                    # ty = ty.flatten()
+                    # py = np.append(ty,py)
+                    # dg.clear_canvas()
+                    # dg.draw_game_from_nparray(py)
+                counter += 1
+            print(i, " Loss: ", calcLoss)
+        counter = 0
 
 
