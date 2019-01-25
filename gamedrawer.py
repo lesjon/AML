d687f5017691b6e14f8f7248b427aa05fc7d9545
Unknown <leonklute@live.nl>
2019-01-25 13:35:02 +0100

    can use arrow keys to go through the frames

--- gamedrawer.py
+++ gamedrawer.py
@@ -22,17 +22,22 @@ class GameDrawer:
     frame_count = 0
     n_frames = 10
     timer = 0
+    json_data = []
+    fragment_n = 0
+    frame_n = 0
 
     def __init__(self):
-
-
         self.master = Tk()
 
         self.scroll_bar = Scrollbar(self.master, orient="horizontal")
         self.scroll_bar.pack()
+        self.text_frame = Frame(self.master)
+        self.FPS_counter = Text(self.text_frame, width=10, height=1, borderwidth=0)
+        self.FPS_counter.pack(side=RIGHT)
 
-        self.FPS_counter = Text(self.master, width=10, height=1, borderwidth=0)
-        self.FPS_counter.pack(anchor="nw")
+        self.fragment_frame = Text(self.text_frame, width=30, height=1, borderwidth=0)
+        self.fragment_frame.pack(side=LEFT)
+        self.text_frame.pack(anchor="n")
 
         self.canvas = Canvas(self.master, width=self.width, height=self.height, bg="green", xscrollcommand=self.scroll_bar.set)
         self.field_image = PhotoImage(file="images/field.gif")
@@ -40,8 +45,30 @@ class GameDrawer:
         self.canvas.pack()
 
         self.scroll_bar.config(command=self.canvas.xview)
-
+        self.master.bind_all('<Left>', self.arrow_press)
+        self.master.bind_all('<Right>', self.arrow_press)
         self.update()
+
+    def arrow_press(self, event):
+        if event.keysym == 'Left':
+            if self.frame_n is not 0:
+                self.frame_n -= 1
+            else:
+                if self.fragment_n is not 0:
+                    self.fragment_n -= 1
+                    self.frame_n = len(self.json_data[self.fragment_n]) - 1
+        if event.keysym == 'Right':
+            if self.frame_n < len(self.json_data[self.fragment_n]) - 1:
+                self.frame_n += 1
+            else:
+                if self.fragment_n < len(self.json_data[self.fragment_n][self.fragment_n]) - 1:
+                    self.fragment_n += 1
+                    self.frame_n = 0
+        self.clear_canvas()
+        self.draw_json(self.json_data[self.fragment_n][self.frame_n])
+        self.fragment_frame.delete(1.0, END)
+        self.fragment_frame.insert(INSERT, "Fragment:" + str(self.fragment_n) + ",frame:" + str(self.frame_n))
+
 
     def update(self):
         self.master.update()
@@ -89,38 +116,34 @@ class GameDrawer:
             # Draw frame
             self.draw_json(frame)
 
-    def draw_game_from_json(self, json_file):
-        """
-        This function is to play a whole match
-        :param json_file: a file containing frames from a game
-        :return:
-        """
+    def draw_game_from_json(self):
+        # This function is to play a whole match
         print("draw game from json")
-        for s, json_object in enumerate(json_file):
-            self.canvas.xview_moveto(s/len(json_file))
-            self.draw_json(json_object)
-            self.clear_canvas()
-            # time.sleep(1/60)
-            '''self.master.after(1000/60, self.draw_game_from_json)
-            yield'''
+        for fragment in self.json_data:
+            for json_object in fragment:
+                self.draw_json(json_object)
+                self.clear_canvas()
+                # time.sleep(1/60)
+                '''self.master.after(1000/60, self.draw_game_from_json)
+                yield'''
 
     def draw_json(self, json_data, dash=None):
         """
         This function draws one frame in json/ dict format
         :param json_data: one frame in a dict/ json
-        :param dasg: show these with this dash
+        :param dash: show these with this dash
         :return:
         """
         def draw_object(game_object, size, color):
-            y_vel_vector_factor = 45
-            x_vel_vector_factor = 60
-            vel_vector_factor = 2  # 0.01
+            # y_vel_vector_factor = 45
+            # x_vel_vector_factor = 60
+            # vel_vector_factor = 2  # 0.01
             x_pos = (game_object['x'] + self.field_width / 2) * self.width / self.field_width
             y_pos = (game_object['y'] + self.field_height / 2) * self.height / self.field_height
             x_vel = game_object['x_vel']  #  + self.field_width / 2) * self.width / self.field_width
             y_vel = game_object['y_vel']  # + self.field_height / 2) * self.height / self.field_height
-            y_vel *= y_vel_vector_factor * vel_vector_factor
-            x_vel *= x_vel_vector_factor * vel_vector_factor
+            # y_vel *= y_vel_vector_factor * vel_vector_factor
+            # x_vel *= x_vel_vector_factor * vel_vector_factor
             self.delete_each_frame.append(create_circle(self.canvas, x_pos, y_pos, size, fill=color, dash=dash))
             self.delete_each_frame.append(self.canvas.create_line(x_pos, y_pos, x_pos + x_vel, y_pos + y_vel, width=1, dash=dash))
 
@@ -149,3 +172,12 @@ class GameDrawer:
             self.FPS_counter.insert(INSERT, str(fps) + "FPS")
             self.timer = time.time()
 
+
+if __name__ == "__main__":
+    # test the code
+    import jsonGameProcessorV2
+    data_reader = jsonGameProcessorV2.JsonGameReader()
+    data_reader.add_file_to_data("Resources/LogsCut/2018-06-18_09-06_ZJUNlict-vs-UMass_Minutebots.json")
+    dg = GameDrawer()
+    dg.json_data = data_reader.json_data
+    dg.wait_till_close()
--- main.py
+++ main.py
@@ -12,29 +12,13 @@ if __name__ == '__main__':
     keep_display_on = True
     play_whole_match = True
 
-
-    # NN_input = jsonGameProcessor.JsonToArray("logs/testWriterOutput.json")
-    # NN_input = jsonGameProcessor.JsonToArray('Resources/Logs/RD_RT.json')
-    # NN_input = jsonGameProcessor.JsonToArray("logs/2018-06-18_09-06_ZJUNlict-vs-UMass_Minutebots.log")
-    # NN_input = jsonGameProcessor.JsonToArray("logs/lstm_creation.json")
-    #
-    #
-    # if play_whole_match:
-    #     for frame in NN_input.data:
-    #         dg.draw_json(NN_input.data_frame_to_dict(frame))
-    #         dg.clear_canvas()
-    #         sleep(1/30)
-
-    # NN_input = jsonGameProcessorV2.JsonToArray("logs/lstm_creation.json")
-    NN_input = lstm_frame_generator.load_data_reader()
+    data_reader = jsonGameProcessorV2.JsonGameReader()
+    data_reader.add_file_to_data("Resources/LogsCut/2018-06-18_09-06_ZJUNlict-vs-UMass_Minutebots.json")
     dg = gamedrawer.GameDrawer()
+    dg.json_data = data_reader.json_data
     sleep(1)
     if play_whole_match:
-        for n, fragment in enumerate(NN_input.data):
-            print(n, "size of fragment:", np.shape(fragment))
-            for frame in fragment:
-                dg.draw_json(NN_input.data_frame_to_dict(frame))
-                dg.clear_canvas()
+        dg.draw_game_from_json()
 
     if keep_display_on:
         dg.wait_till_close()
