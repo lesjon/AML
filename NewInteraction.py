c21dc6528248d92d3e726ab250bb4d29685dd4d6
Nahuel <na.manterola@gmail.com>
2019-01-26 17:20:39 +0100

    Fully working interaction thingy

--- NewInteraction.py
+++ NewInteraction.py
@@ -3,22 +3,37 @@ import numpy as np
 import dataManagement as dm
 
 
-pairedFrames = dm.get_data('Resources/LogsCut/2018-06-19_19-24_CMμs-vs-TIGERs_Mannheim.json', 30)
-np.random.shuffle(pairedFrames)
+# pairedFrames2 = dm.get_data('Resources/LogsCut/2018-06-19_19-24_CMμs-vs-TIGERs_Mannheim.json', 30, 0)
+pairedFrames2 = dm.get_data('Resources/LogsCut/2018-06-21_11-36_TIGERs_Mannheim-vs-ZJUNlict_d.json', 15, 0)
+np.random.shuffle(pairedFrames2)
+
+velocityWeight = np.array([[1.14,0.86,0,0,0,0],
+                           [1.14,0.86,0,0,0,0],
+                           [1.14,0.86,0,0,0,0],
+                           [1.14,0.86,0,0,0,0],
+                           [1.14,0.86,0,0,0,0],
+                           [1.14,0.86,0,0,0,0],
+                           [1.14,0.86,0,0,0,0],
+                           [1.14,0.86,0,0,0,0],
+                           [1.14,0.86,0,0,0,0],
+                           [1.14,0.86,0,0,0,0]
+                           ])
 
 
 def train_on_teammate(bot1, bot2, ball):
     t_input = tf.reshape(tf.concat([bot1, bot2, ball], 0), [1,16])
-    t_hidden1 = tf.layers.dense(inputs=t_input, units=30, activation=tf.nn.tanh)
-    t_hidden2 = tf.layers.dense(inputs=t_hidden1, units=30, activation=tf.nn.tanh)
-    t_output = tf.layers.dense(inputs=t_hidden2, units=6)
+    t_hidden1 = tf.layers.dense(inputs=t_input, units=60, activation=tf.nn.tanh)
+    t_hidden2 = tf.layers.dense(inputs=t_hidden1, units=60, activation=tf.nn.tanh)
+    t_hidden3 = tf.layers.dense(inputs=t_hidden2, units=60, activation=tf.nn.tanh)
+    t_output = tf.layers.dense(inputs=t_hidden3, units=6)
     return tf.to_float(t_output)
 
 def train_on_opponent(bot1, bot2, ball):
     o_input = tf.reshape(tf.concat([bot1, bot2, ball], 0), [1,16])
     o_hidden1 = tf.layers.dense(inputs=o_input, units=30, activation=tf.nn.tanh)
     o_hidden2 = tf.layers.dense(inputs=o_hidden1, units=30, activation=tf.nn.tanh)
-    o_output = tf.layers.dense(inputs=o_hidden2, units=6)
+    o_hidden3 = tf.layers.dense(inputs=o_hidden2, units=60, activation=tf.nn.tanh)
+    o_output = tf.layers.dense(inputs=o_hidden3, units=6)
     return tf.to_float(o_output)
 
 def train_cycle(team1, team2, ball):
@@ -29,23 +44,23 @@ def train_cycle(team1, team2, ball):
     i = tf.constant(0, dtype=tf.int32)
 
     while_condition_outer =     lambda i, state: tf.less(i, t1len)                         # Create while conditions for loops
-    while_condition_team =      lambda j, state: tf.less(j, t1len)
-    while_condition_opponent =  lambda k, state: tf.less(k, t2len)
+    while_condition_team =      lambda i, j, state: tf.less(j, t1len)
+    while_condition_opponent =  lambda i, k, state: tf.less(k, t2len)
 
-    def teamloop(j, state):
-        state = tf.reduce_sum([state, train_on_teammate(tf.gather_nd(team1, [i]), tf.gather_nd(team1, [j]), ball)], 0)
-        return tf.add(j,1), state
+    def teamloop(i, j, state):
+        returnedState = tf.reduce_sum([state, train_on_teammate(tf.gather_nd(team1, [i]), tf.gather_nd(team1, [j]), ball)], 0)
+        return i, tf.add(j,1), returnedState
 
-    def opponentloop(k, state):
-        state = tf.reduce_sum([state, train_on_teammate(tf.gather_nd(team1, [i]), tf.gather_nd(team2, [k]), ball)], 0)
-        return tf.add(k,1), state
+    def opponentloop(i, k, state):
+        returnedState = tf.reduce_sum([state, train_on_teammate(tf.gather_nd(team1, [i]), tf.gather_nd(team2, [k]), ball)], 0)
+        return i, tf.add(k,1), returnedState
 
     def outerloop(i, state):
         j = tf.constant(0, dtype=tf.int32)
         k = tf.constant(0, dtype=tf.int32)
         bot = tf.zeros([1,6], tf.float32)
-        j, bot = tf.while_loop(while_condition_team, teamloop, [j, bot])
-        k, bot = tf.while_loop(while_condition_opponent, opponentloop, [k, bot])
+        i, j, bot = tf.while_loop(while_condition_team, teamloop, [i, j, bot])
+        i, k, bot = tf.while_loop(while_condition_opponent, opponentloop, [i, k, bot])
         state = tf.concat([state, bot], 0)
         return tf.add(i,1), state
 
@@ -66,15 +81,34 @@ with tf.Graph().as_default():
 
     tf_y = train_cycle(tf_t1, tf_t2, tf_ball)               # Estimate by network
 
-    loss = tf.losses.mean_squared_error(tf_l, tf_y)
-    train_op = tf.train.AdamOptimizer(0.000001).minimize(loss)
+    weights = tf.placeholder(tf.float32, shape=(10,6))
 
-    init = train_cycle(pairedFrames[0, 0, 0],pairedFrames[0, 0, 1], pairedFrames[0, 0, 2])
+    loss = tf.losses.mean_squared_error(tf_l, tf_y, weights=tf.slice(weights,[0,0], tf.shape(tf_l)))
+    train_op = tf.train.AdamOptimizer(0.001).minimize(loss)
+
+    init = train_cycle(pairedFrames2[0, 0, 0],pairedFrames2[0, 0, 1], pairedFrames2[0, 0, 2])
+
+    saver = tf.train.Saver()
+
     with tf.Session() as sess:
-
-        print("hoi")
+        # saver.restore(sess, "Saved_models/Interaction/Interaction_h60h60h60_CMus_ZJUNlict")
+        print("Start learning")
         tf.global_variables_initializer().run()
-        for i in range(1,5000):
-            for frame in pairedFrames:
-                calcLoss = sess.run([tf_y], feed_dict={tf_t1: frame[0, 0], tf_t2: frame[0, 1], tf_ball: frame[0, 2], tf_l: frame[1, 0]})
-            print(i, " Loss: ", calcLoss)
+        # ding = sess.run([velocity], feed_dict={tf_t1: pairedFrames2[0][0, 0], tf_t2: pairedFrames2[0][0, 1], tf_ball: pairedFrames2[0][0, 2], tf_l: pairedFrames2[0][1, 0]})
+        # print(ding)
+
+        for i in range(1,50):
+            trainLoss = 0
+            for frame in pairedFrames2[:-200]:
+                _, losst = sess.run([train_op, loss], feed_dict={tf_t1: frame[0, 0], tf_t2: frame[0, 1], tf_ball: frame[0, 2], tf_l: frame[1, 0], weights: velocityWeight})
+                trainLoss += losst
+            trainLoss = trainLoss / len(pairedFrames2[:-200])
+            calcLoss = 0
+            for frame in pairedFrames2[-200:]:
+                losst = sess.run(loss, feed_dict={tf_t1: frame[0, 0], tf_t2: frame[0, 1], tf_ball: frame[0, 2], tf_l: frame[1, 0], weights: velocityWeight})
+                calcLoss = calcLoss + losst
+            calcLoss = calcLoss / len(pairedFrames2[-200:])
+            print(i, "Training Loss: ", trainLoss, "Test Loss:", calcLoss)
+
+        # save_path = saver.save(sess, "Saved_models/Interaction/Interaction_h60h60h60_CMus_ZJUNlict")
+        # print("Model saved in path: %s" % save_path)
--- dataManagement.py
+++ dataManagement.py
@@ -2,7 +2,7 @@ import json
 import numpy as np
 
 # Takes the file from string 'resource' and transforms the data into a 5 dimensional array: dataOrdered[set][frame][team1, team2, ball][robotID][x,y,x_vel,etc]
-def import_file(resource):
+def import_file(resource, order):
     read_file = open(resource, "r")
     data = json.load(read_file)
     dataOrdered = []
@@ -10,25 +10,51 @@ def import_file(resource):
         dataOrdered.append([])
         for frame in set:
             dataOrdered[-1].append([])
-            dataOrdered[-1][-1].append( np.zeros((len(frame["robots_yellow"]), 6)) )
-            for j in range(len(frame["robots_yellow"])):
-                dataOrdered[-1][-1][-1][j,0] = np.float32(frame["robots_yellow"][j]["x"])
-                dataOrdered[-1][-1][-1][j,1] = np.float32(frame["robots_yellow"][j]["y"])
-                dataOrdered[-1][-1][-1][j,2] = np.float32(frame["robots_yellow"][j]["x_vel"]*100)
-                dataOrdered[-1][-1][-1][j,3] = np.float32(frame["robots_yellow"][j]["y_vel"]*100)
-                dataOrdered[-1][-1][-1][j,4] = np.float32(frame["robots_yellow"][j]["x_orien"])
-                dataOrdered[-1][-1][-1][j,5] = np.float32(frame["robots_yellow"][j]["y_orien"])
-            dataOrdered[-1][-1][-1] = np.float32(dataOrdered[-1][-1][-1])
-            dataOrdered[-1][-1].append( np.zeros((len(frame["robots_blue"]), 6)) )
-            for j in range(len(frame["robots_blue"])):
-                dataOrdered[-1][-1][-1][j,0] = np.float32(frame["robots_blue"][j]["x"])
-                dataOrdered[-1][-1][-1][j,1] = np.float32(frame["robots_blue"][j]["y"])
-                dataOrdered[-1][-1][-1][j,2] = np.float32(frame["robots_blue"][j]["x_vel"]*100)
-                dataOrdered[-1][-1][-1][j,3] = np.float32(frame["robots_blue"][j]["y_vel"]*100)
-                dataOrdered[-1][-1][-1][j,4] = np.float32(frame["robots_blue"][j]["x_orien"])
-                dataOrdered[-1][-1][-1][j,5] = np.float32(frame["robots_blue"][j]["y_orien"])
-            dataOrdered[-1][-1][-1] = np.float32(dataOrdered[-1][-1][-1])
+
+            if order == 0:
+                dataOrdered[-1][-1].append( np.zeros((len(frame["robots_yellow"]), 6)) )
+                for j in range(len(frame["robots_yellow"])):
+                    dataOrdered[-1][-1][-1][j,0] = np.float32(frame["robots_yellow"][j]["x"])
+                    dataOrdered[-1][-1][-1][j,1] = np.float32(frame["robots_yellow"][j]["y"])
+                    dataOrdered[-1][-1][-1][j,2] = np.float32(frame["robots_yellow"][j]["x_vel"]*100)
+                    dataOrdered[-1][-1][-1][j,3] = np.float32(frame["robots_yellow"][j]["y_vel"]*100)
+                    dataOrdered[-1][-1][-1][j,4] = np.float32(frame["robots_yellow"][j]["x_orien"])
+                    dataOrdered[-1][-1][-1][j,5] = np.float32(frame["robots_yellow"][j]["y_orien"])
+                dataOrdered[-1][-1][-1] = np.float32(dataOrdered[-1][-1][-1])
+
+                dataOrdered[-1][-1].append( np.zeros((len(frame["robots_blue"]), 6)) )
+                for j in range(len(frame["robots_blue"])):
+                    dataOrdered[-1][-1][-1][j,0] = np.float32(frame["robots_blue"][j]["x"])
+                    dataOrdered[-1][-1][-1][j,1] = np.float32(frame["robots_blue"][j]["y"])
+                    dataOrdered[-1][-1][-1][j,2] = np.float32(frame["robots_blue"][j]["x_vel"]*100)
+                    dataOrdered[-1][-1][-1][j,3] = np.float32(frame["robots_blue"][j]["y_vel"]*100)
+                    dataOrdered[-1][-1][-1][j,4] = np.float32(frame["robots_blue"][j]["x_orien"])
+                    dataOrdered[-1][-1][-1][j,5] = np.float32(frame["robots_blue"][j]["y_orien"])
+                dataOrdered[-1][-1][-1] = np.float32(dataOrdered[-1][-1][-1])
+
+            if order == 1:
+                dataOrdered[-1][-1].append( np.zeros((len(frame["robots_blue"]), 6)) )
+                for j in range(len(frame["robots_blue"])):
+                    dataOrdered[-1][-1][-1][j,0] = np.float32(-frame["robots_blue"][j]["x"])
+                    dataOrdered[-1][-1][-1][j,1] = np.float32(-frame["robots_blue"][j]["y"])
+                    dataOrdered[-1][-1][-1][j,2] = np.float32(-frame["robots_blue"][j]["x_vel"]*100)
+                    dataOrdered[-1][-1][-1][j,3] = np.float32(-frame["robots_blue"][j]["y_vel"]*100)
+                    dataOrdered[-1][-1][-1][j,4] = np.float32(-frame["robots_blue"][j]["x_orien"])
+                    dataOrdered[-1][-1][-1][j,5] = np.float32(-frame["robots_blue"][j]["y_orien"])
+                dataOrdered[-1][-1][-1] = np.float32(dataOrdered[-1][-1][-1])
+
+                dataOrdered[-1][-1].append( np.zeros((len(frame["robots_yellow"]), 6)) )
+                for j in range(len(frame["robots_yellow"])):
+                    dataOrdered[-1][-1][-1][j,0] = np.float32(-frame["robots_yellow"][j]["x"])
+                    dataOrdered[-1][-1][-1][j,1] = np.float32(-frame["robots_yellow"][j]["y"])
+                    dataOrdered[-1][-1][-1][j,2] = np.float32(-frame["robots_yellow"][j]["x_vel"]*100)
+                    dataOrdered[-1][-1][-1][j,3] = np.float32(-frame["robots_yellow"][j]["y_vel"]*100)
+                    dataOrdered[-1][-1][-1][j,4] = np.float32(-frame["robots_yellow"][j]["x_orien"])
+                    dataOrdered[-1][-1][-1][j,5] = np.float32(-frame["robots_yellow"][j]["y_orien"])
+                dataOrdered[-1][-1][-1] = np.float32(dataOrdered[-1][-1][-1])
+
             dataOrdered[-1][-1].append(np.zeros(4))
+
             for j in frame["balls"]:
                 dataOrdered[-1][-1][-1][0] = np.float32(j["x"])
                 dataOrdered[-1][-1][-1][1] = np.float32(j["y"])
@@ -41,10 +67,10 @@ def file_to_x_y(file, frames):
     x = []
     y = []
     for set in file:
-        for frame in set[:-frames]:
-            x.append(frame)
-        for frame in set[frames:]:
-            y.append(frame)
+        for frame in range(len(set[:-frames])):
+            if len(set[frame][0]) == len(set[frame+frames][0]):
+                x.append(set[frame])
+                y.append(set[frame+frames])
     return x, y
 
 def make_pairs(x, y):
@@ -52,8 +78,8 @@ def make_pairs(x, y):
     pairedSet = pairedSet.transpose(1,0,2)
     return pairedSet
 
-def get_data(resource, frames):
-    data = import_file(resource)
+def get_data(resource, frames, order):
+    data = import_file(resource, order)
     x,y = file_to_x_y(data, 30)
     pairs = make_pairs(x,y)
     return pairs
